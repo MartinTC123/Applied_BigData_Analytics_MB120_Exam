11cf5f7e8a83eb3c89fa7ae3e784b20f9f545a82
Martin <martin_candor@hotmail.com>
2023-05-06 21:21:27 +0200

    Added all models the exam.

--- Models.py
+++ Models.py
@@ -1,32 +1,67 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.neural_network import MLPRegressor
+from xgboost import XGBRegressor
+from sklearn.ensemble import AdaBoostRegressor, GradientBoostingRegressor, BaggingRegressor, StackingRegressor
+
 
 class ML_Models():
     def __init__(self):
         pass
 
-    # MODEL
-    def model_Linear(self):
+    def LR(self):
         model = LinearRegression()
-
         return model
 
-    # MODEL
-    def model_DTR(self):
-        #friedmans_mse score for potential splits provided better results than default 
-        #samples required for a leaf node was found best at 5
-        model = DecisionTreeRegressor(criterion="friedman_mse", min_samples_leaf = 5)
-
+    def DTR(self):
+        # Might tweak on the parameters to see different results. Left on default for now.
+        model = DecisionTreeRegressor()
         return model
 
-    def model_MLP(self):
-        model = MLPRegressor(random_state=1, max_iter=500)
-
+    def MLP(self):
+        # Might tweak on the parameters to see different results.
+        model = MLPRegressor(random_state=42, max_iter=500)
         return model
 
+    def XGBoost(self):
+        # Might tweak on the parameters to see different results. Left on default for now.
+        model = XGBRegressor()  
+        return model
 
-    #Ensemble methods: 
-    # Bagging (Bootstrap Aggregating) 
-    # Boosting 
-    # Stacking (Stacked generalization) 
+    def XGBoost_LR(self):
+        # Used "gblinear" as booster param to have Linear Regression as the base learner.
+        # Might tweak on the parameters to see different results.
+        model = XGBRegressor(booster="gblinear")
+        return model
+    
+    def ADA(self):
+        # Might tweak on the parameters to see different results.
+        model = AdaBoostRegressor(random_state=42)
+        return model
+    
+    def GBR(self):
+        # Might tweak on the parameters to see different results. Left on default for now.
+        model = GradientBoostingRegressor()
+        return model
+    
+    def Bagging(self):
+        # Might tweak on the parameters to see different results. Left on default for now.
+        model = BaggingRegressor()
+        return model
+    
+    # Stacking
+    def SER(self):
+        estimators = []
+        estimators.append(("LR", self.LR()))
+        estimators.append(("DTR", self.DTR()))
+        estimators.append(("MLP", self.MLP()))
+        estimators.append(("XGBoost", self.XGBoost()))
+        #estimators.append(("XGBoost_LR", self.XGBoost_LR)) blir ikke gjenkjent som en regressor av cross-validate.
+        estimators.append(("ADA", self.ADA()))
+        estimators.append(("GRB", self.GBR()))
+        estimators.append(("Bagging", self.Bagging()))
+
+        model = StackingRegressor(estimators=estimators)
+        return model  
+
+    # NOTE: Trenger vi ADA_MLP?
